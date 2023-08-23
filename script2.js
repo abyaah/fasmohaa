3edd52c2dcdbe1be5cd1d9eaa388a9e22610dd6e
AK1239 <akilkhatri235@gmail.com>
2023-08-23 11:44:40 +0300

    updated

--- script2.js
+++ script2.js
@@ -1,9 +1,19 @@
 let scrollButton = document.getElementsByClassName("gotop")[0]; // Access the first element
 var footerElement = document.querySelector(".footer");
 var footerHeight = footerElement.clientHeight;
+var topElement = document.querySelector(".part1");
+var topHeight = topElement.clientHeight + 100;
 let scrollHeight = document.body.clientHeight - footerHeight - 800; // Adjust this value to the desired scroll height
 
 window.addEventListener("scroll", function () {
+  if (window.innerWidth <= 560) {
+    if (window.scrollY > scrollHeight || window.scrollY < topHeight) {
+      scrollButton.style.opacity = 0; // Hide the button
+    } else {
+      scrollButton.style.opacity = 1; // Show the button
+    }
+  }
+
   if (window.scrollY > scrollHeight) {
     scrollButton.style.opacity = 0; // Hide the button
   } else {
